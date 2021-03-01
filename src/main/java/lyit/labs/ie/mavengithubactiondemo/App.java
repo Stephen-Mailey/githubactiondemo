40fddabc0ed05f152150d79ca0d32c7ca5ef2e73
Stephen <L00137623@student.lyit.ie>
2021-03-01 17:57:50 +0000

    initial files added.

---
+++ src/main/java/lyit/labs/ie/mavengithubactiondemo/App.java
@@ -0,0 +1,14 @@
+package lyit.labs.ie.mavengithubactiondemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("This is the GitHub Actions Demo!!");
+    }
+}
